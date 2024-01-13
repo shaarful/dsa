aa31f8b87486e869df6ecaf563e3995e818e8999
sharful09 <sharful09@gmail.com>
2024-01-13 20:17:47 +0600

    binary search tree completed

--- bst.py
+++ bst.py
@@ -4,6 +4,12 @@ class BinarySearchTreeNode:
         self.left = None
         self.right = None
 
+    def __repr__(self):
+        return self.data
+
+    def __str__(self):
+        return self.data
+
     def add_child(self, data):
         if self.data == data:
             return
@@ -27,6 +33,78 @@ class BinarySearchTreeNode:
             elements += self.right.in_order_traversal()
         return elements
 
+    def post_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
+        if self.right:
+            elements += self.right.in_order_traversal()
+        elements.append(self.data)
+        return elements
+
+    def pre_order_traversal(self):
+        elements = [self.data]
+        if self.left:
+            elements += self.left.in_order_traversal()
+        if self.right:
+            elements += self.right.in_order_traversal()
+        return elements
+
+    def search(self, data):
+        if self.data == data:
+            return True
+        if data < self.data:
+            if self.left:
+                return self.left.search(data)
+            else:
+                return False
+        if data > self.data:
+            if self.right:
+                return self.right.search(data)
+            else:
+                return False
+
+    def find_min(self):
+        if self.left:
+            return self.left.find_min()
+        else:
+            return self.data
+
+    def find_max(self):
+        if self.right:
+            return self.right.find_max()
+        else:
+            return self.data
+
+    def calculate_sum(self):
+        total = 0
+        if self.left:
+            total += self.left.calculate_sum()
+        if self.right:
+            total += self.right.calculate_sum()
+        return total + self.data
+
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+
+            max_val = self.left.find_max()
+            self.data = max_val
+            self.left = self.left.delete(max_val)
+
+        return self
+
 
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
@@ -40,3 +118,11 @@ if __name__ == '__main__':
     tree = build_tree(numbers)
     sorted_number = tree.in_order_traversal()
     print(sorted_number)
+    print(tree.search(3))
+    print(tree.find_min())
+    print(tree.find_max())
+    print(tree.calculate_sum())
+    print(tree.post_order_traversal())
+    print(tree.pre_order_traversal())
+    tree.delete(23)
+    print(tree.in_order_traversal())
