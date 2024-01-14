399f3ba19d930c14d5992bb32652fa27ace73247
Sharful Islam <34204530+sharful09@users.noreply.github.com>
2024-01-14 15:55:25 +0600

    graph finished

--- graph.py
+++ graph.py
@@ -12,15 +12,37 @@ class Graph:
         print(self.graph_dict)
 
     def get_paths(self, start, end, path=[]):
-        path += [start]
+        path = path + [start]
         if start == end:
             return [path]
         if start not in self.graph_dict:
             return []
 
+        paths = []
         for node in self.graph_dict[start]:
             if node not in path:
-                pass
+                new_paths = self.get_paths(node, end, path)
+                for new_path in new_paths:
+                    paths.append(new_path)
+
+        return paths
+
+    def get_shortest_path(self, start, end, path=[]):
+        path = path + [start]
+
+        if start not in self.graph_dict:
+            return None
+        if start == end:
+            return path
+
+        shorted_path = None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp = self.get_shortest_path(node, end, path)
+            if sp:
+                if shorted_path is None or len(sp) < len(shorted_path):
+                    shorted_path = sp
+        return shorted_path
 
 
 if __name__ == '__main__':
@@ -33,6 +55,7 @@ if __name__ == '__main__':
         ("New York", "Toronto"),
     ]
     start = 'Mumbai'
-    end = 'Mumbai'
+    end = 'New York'
     route_graph = Graph(routes)
     print(route_graph.get_paths(start, end))
+    print(route_graph.get_shortest_path(start, end))
