473fee427e7063fa8814b2d8208089701f3fa633
sharful09 <sharful09@gmail.com>
2024-06-07 16:36:40 +0600

    new start

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,14 @@
+def insertion_sort(elements):
+    for i in range(1, len(elements)):
+        anchor = elements[i]
+        j = i - 1
+        while j >= 0 and anchor < elements[j]:
+            elements[j + 1] = elements[j]
+            j -= 1
+        elements[j + 1] = anchor
+
+
+if __name__ == '__main__':
+    numbers = [21, 12, 32, 24, 23, 22, 57, 16]
+    insertion_sort(numbers)
+    print(numbers)
